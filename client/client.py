85283f7c0d2dccf9e82fe22bacef11a4822cd264
Aydin Azari Farhad <aidin.azarifarhad@gmail.com>
2022-04-29 00:31:20 -0400

    Fixed formatting and print functions

--- client/client.py
+++ client/client.py
@@ -19,10 +19,14 @@ newFileNameIndex = 3
 BUFFER_SIZE = 4096
 
 # takes file name (splitCmd[1]) as input and returns the length of the name of the file in 5 binary digits
+
+
 def fileNameLength(file):
     return (bin(len(file))[2:].zfill(5))
 
 # takes file name as input and returns the file name as set of bytes
+
+
 def fileNameToBin(fileName):
     name = ''
     for i in fileName:
@@ -30,10 +34,14 @@ def fileNameToBin(fileName):
     return name
 
 # takes file name as input, and returns the file size in Bytes [Note: NEEDS TO BE 4 BYTES]
+
+
 def getFileSize(file):
     return (bin(os.path.getsize(file)))[2:].zfill(32)
 
 # Command Fucntions
+
+
 def put(fileName):
     print("Uploading to server...")
     opCode = "000"
@@ -41,22 +49,25 @@ def put(fileName):
     FN = fileNameToBin(fileName)
     FS = getFileSize(fileName)
     toSend = bytes(f"{opCode}{FL}{FN}{FS}", encoding="utf-8")
-    print (f"ToSend: {toSend}")
+    print(f"ToSend: {toSend}")
     clientSocket.sendall(toSend)
     with open(fileName, "rb") as file:
         while True:
             buffer = file.read(BUFFER_SIZE)
-            print (f"Buffer: {buffer}")
+            print(f"Buffer: {buffer}")
             if not buffer:
                 break
             clientSocket.sendall(buffer)
     print("Upload Complete.")
 
+
 def get():
     print("Downloading from server...")
 
+
 def help():
     print("Asking for help")
+
 
 def bye():
     clientSocket.close()
@@ -64,6 +75,8 @@ def bye():
     quit()
 
 # Interprets the user command
+
+
 def getCmd(splitCmd):
     if ("put" in splitCmd):
         put(splitCmd[fileNameIndex])
@@ -76,6 +89,7 @@ def getCmd(splitCmd):
 
 ###############################
 
+
 # Takes IP and port number from user
 serverIP = "192.168.2.18"  # input("Please enter server IP address: ")
 port = 15000  # int(input("Please enter port number: "))
@@ -87,7 +101,7 @@ clientSocket.connect((serverIP, port))
 print("Connection Established")
 
 # Takes user command
-cmd = "put test1.txt" #input("Client:~\$ ")
+cmd = "put test1.txt"  # input("Client:~\$ ")
 # splits user input based on space chars into arrays
 splitCmd = cmd.split()
 
